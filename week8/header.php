e9da584df1b753c0cce8ac40a1209cfcae4a7343
Alejandra Diaz <diaz.rivera.alejandra@gmail.com>
2015-06-20 15:44:21 -0400

    june-20-class

---
+++ week8/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TheAdventuresofAle</title>	
+	<link rel="stylesheet" type="text/css" href="reset.css">
+	<link href="<?php bloginfo( 'stylesheet_url' ); ?>" rel="stylesheet" media="all" type="text/css" />
+	<link href='http://fonts.googleapis.com/css?family=Oswald:400,700,300' rel='stylesheet' type='text/css'>
+	<meta name="viewport" content="width=device-width, initialscale=1">
+
+</head>
+
+<div id="wrapper">
+<body>
+
+	<div class="clearfix" id="top-header">
+		<img src="headimage.jpg" alt="Ale">
+		<div id="logo">
+			<h1>The Adventures of Ale</h1>
+			<h2 id="h2-header">Stories Narrated by Alejandra Diaz</h2>
+
+		</div>
+	</div>
